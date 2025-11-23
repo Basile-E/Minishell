6d8eb2621cedfef85deeea9967cc5efe7270218f
Basile Ecolivet <baecoliv@f2r9s13.clusters.42paris.fr>
2025-11-24 00:23:57 +0100

    clean

--- build/exec/builtins/cd_utils.d
+++
@@ -1,2 +0,0 @@
-build/exec/builtins/cd_utils.o: exec/builtins/cd_utils.c \
-  includes/minishell.h includes/../libft/libft.h
--- build/parsing/token/token_mgmt.d
+++
@@ -1,2 +0,0 @@
-build/parsing/token/token_mgmt.o: parsing/token/token_mgmt.c \
-  includes/../includes/minishell.h includes/../libft/libft.h
